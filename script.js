c1efdeb2097c11eddc661ca48241d1af4286b650
diccord1 <diccord1@gmail.com>
2025-09-04 11:28:09 +0000

    comitee

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ document.addEventListener("DOMContentLoaded", () => {
             .catch(error => console.error("Erro:", error));
     });
 });
+
+/* === LOADER === */
+
+const MIN_TIME = 1350;
+window.addEventListener("load", () => {
+    const loader = document.getElementById("loader");
+
+    const startTime = performance.timing.navigationStart;
+    const elapsed = Date.now() - startTime;
+    const remaining = MIN_TIME - elapsed;
+
+    setTimeout(() => {
+        document.body.classList.add("loaded");
+
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 800);
+    }, remaining > 0 ? remaining : 0);
+});
